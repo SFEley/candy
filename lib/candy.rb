a2c05e4190199cf56c3b9c6f5cdf6571b5b7e9b5
Stephen Eley <sfeley@gmail.com>
2010-01-20 17:50:35 -0500

    Object updating and collections

--- lib/candy.rb
+++ lib/candy.rb
@@ -3,18 +3,87 @@ require 'candy/crunch'
 
 # Mix me into your classes and Mongo will like them!
 module Candy
+  
   module ClassMethods
+    include Crunch::ClassMethods
+    
+    # Retrieves an object from Mongo by its ID and returns it.  Returns nil if the ID isn't found in Mongo.
+    def find(id)
+      if collection.find_one(id)
+        self.new({:_candy => id})
+      end
+    end
+    
+    # Retrieves a single object from Mongo by its search attributes, or nil if it can't be found.
+    def first(conditions={})
+      options = extract_options(conditions)
+      if record = collection.find_one(conditions, options)
+        find(record["_id"])
+      else
+        nil
+      end
+    end
+    
+    # Retrieves all objects matching the search attributes as an Enumerator (even if it's an empty one).
+    # The option fields from Mongo::Collection#find can be passed as well, and will be extracted from the 
+    # condition set if they're found.
+    def all(conditions={})
+      options = extract_options(conditions)
+      cursor = collection.find(conditions, options)
+      Enumerator.new do |yielder|
+        while record = cursor.next_document do
+          yielder.yield find(record["_id"])
+        end
+      end  
+    end
+    
+  private
+    # Returns a hash of options matching those enabled in Mongo::Collection#find, if any of them exist
+    # in the set of search conditions.
+    def extract_options(conditions)
+      options = {:fields => []}
+      [:fields, :skip, :limit, :sort, :hint, :snapshot, :timeout].each do |option|
+        options[option] = conditions.delete(option) if conditions[option]
+      end
+      options
+    end
     
   end
   
   module InstanceMethods
+    include Crunch::InstanceMethods
+    
+    # We push ourselves into the DB before going on with our day.
     def initialize(*args, &block)
-      @__candy = self.class.collection.insert({})
+      @__candy = check_for_candy(args) || self.class.collection.insert({})
       super
     end
 
+    # Shortcut to the document ID.
     def id
-      @id
+      @__candy
+    end
+    
+    # Candy's magic ingredient. Assigning to any unknown attribute will push that value into the Mongo collection.
+    # Retrieving any unknown attribute will return that value from this record in the Mongo collection.
+    def method_missing(name, *args, &block)
+      if name =~ /(.*)=$/  # We're assigning
+        self.class.collection.update({"_id" => @__candy}, {"$set" => {$1 => args[0]}})
+      else
+        self.class.collection.find_one(@__candy, :fields => [name.to_s])[name.to_s]
+      end
+    end
+    
+  private
+  
+    # Returns the secret decoder ring buried in the arguments to "new"
+    def check_for_candy(args)
+      if (candidate = args.pop).is_a?(Hash) and candidate[:_candy]
+        candidate[:_candy]
+      else # This must not be for us, so put it back  
+        args.push candidate if candidate
+        nil
+      end
     end
     
   end
--- spec/candy_spec.rb
+++ spec/candy_spec.rb
@@ -6,12 +6,113 @@ describe "Candy" do
     include Candy
   end
   
+  before(:all) do
+    @verifier = Zagnut.collection
+  end
+  
   before(:each) do
     @this = Zagnut.new
   end
   
+  
   it "inserts a document immediately" do
     @this.id.should be_a(Mongo::ObjectID)
+  end
+
+  it "saves any attribute it doesn't already handle to the database" do
+    @this.bite = "Tasty!"
+    @verifier.find_one["bite"].should == "Tasty!"
+  end
+
+  it "retrieves any attribute it doesn't already know about from the database" do
+    @verifier.update({:_id => @this.id}, {:chew => "Yummy!", :bite => "Ouch."})
+    @this.chew.should == "Yummy!"
+  end
+
+  it "can roundtrip effectively" do
+    @this.swallow = "Gulp."
+    @this.swallow.should == "Gulp."
+  end
+
+  it "handles missing attributes gracefully" do
+    @this.licks.should == nil
+  end
+  
+  it "allows multiple attributes to be set" do
+    @this.licks = 7
+    @this.center = 0.5
+    @this.licks.should == 7
+  end
+  
+  describe "retrieval" do
+    it "can find a record by its ID" do
+      @this.licks = 10
+      that = Zagnut.find(@this.id)
+      that.licks.should == 10
+    end
+    
+    it "roundtrips across identical objects" do
+      that = Zagnut.find(@this.id)
+      @this.calories = 7500
+      that.calories.should == 7500
+    end
+    
+    it "returns nil on an object that can't be found" do
+      id = Mongo::ObjectID.new
+      Zagnut.find(id).should be_nil
+    end
+    
+    it "can get a single object by attributes" do
+      @this.pieces = 7.5
+      @this.color = "red"
+      that = Zagnut.first("pieces" => 7.5)
+      that.color.should == "red"
+    end
+    
+    it "returns nil if a first object can't be found" do
+      @this.pieces = 11
+      Zagnut.first("pieces" => 5).should be_nil
+    end
+  end
+  
+  describe "collections" do
+    before(:each) do
+      @this.color = "red"
+      @this.weight = 11.8
+      @that = Zagnut.new
+      @that.color = "red"
+      @that.pieces = 6
+      @that.weight = -5
+      @the_other = Zagnut.new
+      @the_other.color = "blue"
+      @the_other.pieces = 7
+      @the_other.weight = 0
+    end
+    
+    it "can get all objects in a collection" do
+      those = Zagnut.all
+      those.count.should == 3
+    end
+    
+    it "can get all objects matching a search condition" do
+      those = Zagnut.all(:color => "red")
+      those.count.should == 2
+    end
+    
+    it "still returns if nothing matches" do
+      Zagnut.all(:color => "green").to_a.should == []
+    end
+    
+    it "can take options" do
+      those = Zagnut.all(:color => "red", :sort => ["weight", :asc])
+      those.collect{|z| z.weight}.should == [-5, 11.8]
+    end
+      
+  end
+  
+  
+  after(:each) do
+    Zagnut.collection.remove
   end
   
 end
